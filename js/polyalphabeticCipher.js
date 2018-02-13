ef8a12425b89687007d4a0dac3ced5c846fa0d00
Piotr Handkowski <piotrhandkowski@Piotrs-MacBook-Pro.local>
2018-02-13 12:45:48 +0000

    initial commit

---
+++ js/polyalphabeticCipher.js
@@ -0,0 +1,164 @@
+
+var levelA = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var levelB = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a"];
+var levelC = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b"];
+var levelD = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"];
+var levelE = ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"];
+var levelF = ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"];
+var levelG = ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"];
+var levelH = ["h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g"];
+var levelI = ["i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h"];
+var levelJ = ["j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i"];
+var levelK = ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+var levelL = ["l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+var levelM = ["m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
+var levelN = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+var levelO = ["o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+var levelP = ["p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"];
+var levelQ = ["q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
+var levelR = ["r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"];
+var levelS = ["s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"];
+var levelT = ["t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"];
+var levelU = ["u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
+var levelV = ["v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"];
+var levelW = ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"];
+var levelX = ["x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"];
+var levelY = ["y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"];
+var levelZ = ["z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"];
+
+
+var polyalphabeticTable = [levelA, levelB, levelC, levelD, levelE, levelF, levelG, levelH, levelI, levelJ, levelK, levelL, levelM, levelN, levelO, levelP, levelQ, levelR, levelS, levelT, levelU, levelV, levelW, levelX, levelY, levelZ];
+
+
+
+// returns the positon of the letter to be encoded
+function letterPosition(letter){
+  for (var i = 0; i < 26; i++) {
+    if(levelA[i] == letter){
+      return i;
+    }
+  }
+}
+// returns a position of encoded letter given the level
+function encodedLetterPosition(letter, level){
+  for (var i = 0; i < 26; i++) {
+    if(polyalphabeticTable[level][i] == letter){
+      return i;
+    }
+  }
+}
+
+// changes the key (word) into array of positions
+function decodeKey(key){
+  var decodedKey = [];
+  for (var i = 0; i < key.length; i++) {
+    decodedKey.push(letterPosition(key[i]));
+  }
+  console.log("decodeKey method reutrns" + decodedKey)
+  return decodedKey;
+}
+
+
+// encodes a message according to provided key
+// works similar to Caesar cipher but instead of just one number it uses a combination of them
+function encodeMessage(message, key){
+  var result = "";
+  var decodedKey = decodeKey(key);
+  console.log(decodedKey);
+  // console.log(decodedKey);
+  var index = 0; //  the position of the number from the key
+  var keyLastPosition = key.length - 1;
+  for (var i = 0; i < message.length; i++) {
+    var keyPosition = decodedKey[index]; // represents the appropriate table to be used (from 26 tables)
+    console.log("keyPosition = " +keyPosition);
+    console.log("char to be encoded "+message[i]);
+    if(message[i] == " "){ // spaces are not encripted
+      result = result.concat(message[i]);
+      continue;
+    }
+    var letterPos = letterPosition(message[i]);
+    console.log(letterPos);
+    // selects a letter from the correct table at the same position as the letter to be encoded
+    var encodedChar = polyalphabeticTable[keyPosition][letterPos];
+    console.log("encodedChar " +encodedChar);
+    result += encodedChar;
+    if(index == keyLastPosition){
+      index = 0;
+    }else{
+      index++;
+    }
+    console.log("index " + index);
+  }
+  return result;
+}
+
+
+function decodeMessage(message, key){
+  var result = "";
+  var decodedKey = decodeKey(key);
+  console.log(decodedKey);
+  var index = 0;
+
+  var keyLastPosition = key.length - 1;
+  console.log("key last position " +keyLastPosition);
+  for (var i = 0; i < message.length; i++) {
+      console.log("index " + index)
+    var keyPosition = decodedKey[index]; // checks which table was used to encode the letter
+    console.log(message[i]);
+    console.log("keyPosition "+keyPosition);
+    if(message[i] == " "){
+      result = result.concat(message[i]);
+      continue;
+    }
+    var letterPos = encodedLetterPosition(message[i], keyPosition); // checks at which position was the encoded letter
+    console.log(letterPos);
+    var decodedChar = polyalphabeticTable[0][letterPos]; // selects a letter from the 1st (not encoded) table
+    console.log(decodedChar);
+    result += decodedChar;
+    if(index == keyLastPosition){
+      index = 0;
+    }else{
+      index++;
+    }
+  }
+  return result;
+}
+
+
+
+
+function cipherButtonFunction(){
+  var enteredKey = document.getElementById('enteredKey').value;
+  console.log(enteredKey);
+  var message = document.getElementById("inputMessage").value;
+  console.log(message);
+  if(enteredKey == "" || message == ""){
+    alert("Please enter key and message to de ciphered/deciphered!");
+    return;
+  }
+
+
+  var result = encodeMessage(message, enteredKey);
+  document.getElementById("result").innerHTML = result;
+
+
+}
+
+function decipherButtonFunction(){
+  var enteredKey = document.getElementById('enteredKey').value;
+  console.log(enteredKey);
+  var message = document.getElementById("inputMessage").value;
+  console.log(message);
+
+  if(enteredKey == "" || message == ""){
+    alert("Please enter key and message to de ciphered/deciphered!");
+    return;
+  }
+
+  var result = decodeMessage(message, enteredKey);
+  document.getElementById("result").innerHTML = result;
+}
+
+// document.getElementsByClassName('cipherMethod').onclick = cipherButtonFunction();
+// document.getElementsByClassName('decipherMethod').onclick = decipherButtonFunction();
+// document.getElementsByClassName('enigmaButton').onclick = encriptDecriptMessage();
---
+++ js/zip.js
@@ -0,0 +1,73 @@
+
+// reverses a string
+function reverseString(string){
+  var reversedString = "";
+  for (var i = string.length -1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
+// divides a string into two equal parts. If the string length is odd it adds a space at the end
+// returns two length equal strings
+function divide(string){
+  if(string.length % 2 != 0){
+    string += " ";
+  }
+  var middle = (string.length / 2);
+  return [string.substring(0, middle), string.substring(middle, string.length)];
+}
+
+// splits a string in half and zips them together after they have been reversed
+function zip(string){
+  var string1 = reverseString(divide(string)[0]);
+  var string2 = reverseString(divide(string)[1]);
+  console.log(string1);
+  console.log(string2);
+  var result = "";
+  for (var i = 0; i < string1.length; i++) {
+    result += string1[i]+string2[i];
+  }
+  return result;
+}
+
+// unzips the encoded message, reverses the two strings and joins them together
+function unzip(string){
+  var string1 = "";
+  var string2 = "";
+  for (var i = 0; i < string.length; i++) {
+    if(i % 2 == 0){
+      string1 += string[i];
+    }else {
+      string2 += string[i];
+    }
+  }
+  console.log(string1);
+  console.log(string2);
+
+  return reverseString(string1) + reverseString(string2);
+}
+
+function zipMessage(){
+  var message = document.getElementById("inputMessage").value;
+  if(message == ""){
+    alert("Please enter a message to zip/unzip!");
+    return;
+  }
+  var result = zip(message);
+  document.getElementById("result").innerHTML = result;
+}
+
+// document.getElementsByClassName('zipMethod').onclick = zipMessage();
+
+function unzipMessage(){
+  var message = document.getElementById("inputMessage").value;
+  if(message == ""){
+    alert("Please enter a message to zip/unzip!");
+    return;
+  }
+  var result = unzip(message);
+  document.getElementById("result").innerHTML = result;
+}
+
+// document.getElementsByClassName('unzipMethod').onclick = unzipMessage();
